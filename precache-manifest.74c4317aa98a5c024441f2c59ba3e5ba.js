a20ba250104858bd1e08fb0aa54cbfbfc191e291
Ashmit <ashmitchauhan899@gmail.com>
2023-08-19 03:33:35 +0530

    Updates

---
+++ precache-manifest.74c4317aa98a5c024441f2c59ba3e5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e8769b3367a25ff387c",
+    "url": "/amazon-clone/static/css/main.78449576.chunk.css"
+  },
+  {
+    "revision": "6e8769b3367a25ff387c",
+    "url": "/amazon-clone/static/js/main.6e8769b3.chunk.js"
+  },
+  {
+    "revision": "ed197c0f677114f68a6f",
+    "url": "/amazon-clone/static/js/1.ed197c0f.chunk.js"
+  },
+  {
+    "revision": "07834e89587d21403aee",
+    "url": "/amazon-clone/static/js/2.07834e89.chunk.js"
+  },
+  {
+    "revision": "c0ad61c4ccfe8b0ee266",
+    "url": "/amazon-clone/static/js/runtime~main.c0ad61c4.js"
+  },
+  {
+    "revision": "d8abe166f872919674e3c9fa75bafb47",
+    "url": "/amazon-clone/index.html"
+  }
+];
